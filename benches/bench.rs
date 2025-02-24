2634a4e266847224816ad352090d0031a1304f68
newfla <newfla@hotmail.it>
2025-02-24 11:45:44 +0100

    2024 edition update

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
 
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
